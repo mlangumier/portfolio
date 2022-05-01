c925572ada57212c8a0dea29f18f526c192e837b
mlangumier <mathieu.langumier@gmail.com>
2022-05-01 17:49:19 +0200

    Fixed store setup

--- src/Redux/User/userSlice.ts
+++ src/Redux/User/userSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import type { RootState } from './store'
+import type { RootState } from 'Redux/store'
 
 interface UserState {
   id: string,
@@ -19,14 +19,15 @@ export const userSlice = createSlice({
   name: 'user', 
   initialState,
   reducers: {
-    createUser: (state, action: PayloadAction<UserState>) => {
+    initUser: (state, action: PayloadAction<UserState>) => {
+      // Get localStorage/state-storage or ask if has user (yes: find / no: create or let us create + reroll)
       state.id = action.payload.id;
       state.name = action.payload.name;
     }
   }
 })
 
-export const { createUser } = userSlice.actions
+export const { initUser } = userSlice.actions
 export const selectUser = (state: RootState) => state.user
 
 export default userSlice.reducer
--- src/Redux/reducers.ts
+++ src/Redux/reducers.ts
@@ -1,4 +1,4 @@
-import userReducer from './userSlice'
+import userReducer from './User/userSlice'
 
 export const rootReducer = {
   user: userReducer,
