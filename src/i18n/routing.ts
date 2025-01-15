03b27dc23efea2dfac6680d96717fcc77eeb38b7
Mathieu <mathieu.langumier@gmail.com>
2025-01-15 12:50:34 +0100

    Disabled localeDetection to set french as defaultLocale

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -4,6 +4,7 @@ import { defineRouting } from 'next-intl/routing';
 export const routing = defineRouting({
   locales: ['fr', 'en'],
   defaultLocale: 'fr',
+  localeDetection: false,
   pathnames: {
     '/': '/',
     '/about': {
